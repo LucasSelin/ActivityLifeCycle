8026a4ddf3e1f30329901a18e94da218ecd91525
lucas.selin <lucas.selin@aluno.ifsp.edu.br>
2026-08-26 20:11:55 -0300

    Initial commit

---
+++ app/src/main/java/com/example/mainscreen/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.mainscreen
+
+import android.os.Bundle
+import android.renderscript.Sampler.Value
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.mainscreen.ui.theme.MainScreenTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MainScreenTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MainScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier, mainViewModel: MainViewModel = viewModel()) {
+    var name by remember { mutableStateOf("") }
+    var age = remember { mutableStateOf("") }
+
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+        TextField(
+            value = mainViewModel.name,
+            label = { Text("Name") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+            modifier = Modifier.fillMaxWidth(),
+            onValueChange = { mainViewModel.updateName(it)}
+        )
+        TextField(
+            value = mainViewModel.age.toString(),
+            label = { Text("Age")},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth(),
+            onValueChange = { mainViewModel.udapteAge(it.toInt()) }
+        )
+    }
+
+    Text(
+        text = "Hello",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    MainScreenTheme {
+        MainScreen()
+    }
+}
