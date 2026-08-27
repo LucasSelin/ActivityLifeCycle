391df8f54c150f689000bc8666ae1b7533c596a8
lucas.selin <lucas.selin@aluno.ifsp.edu.br>
2026-08-26 21:47:12 -0300

    feat(mainviewmodel): adicionando uistate final

--- app/src/main/java/com/example/mainscreen/MainViewModel.kt
+++ app/src/main/java/com/example/mainscreen/MainViewModel.kt
@@ -4,21 +4,29 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.SavedStateViewModelFactory
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
-class MainViewModel : ViewModel() {
-    private val _uiState = MutableStateFlow(User())
+class MainViewModel(val savedStateHandle: SavedStateHandle) : ViewModel() {
+    private companion object {
+        const val USER_KEY = "user"
+    }
+
+    private val _uiState = MutableStateFlow(savedStateHandle[USER_KEY] ?: User())
     var uiState: StateFlow<User> = _uiState.asStateFlow()
 
     fun updateName(name: String) {
         _uiState.update { it.copy(name = name) }
+        savedStateHandle[USER_KEY] = _uiState.value.copy(name = name)
     }
 
     fun udapteAge(age: Int) {
         _uiState.update { it.copy(age = age) }
+        savedStateHandle[USER_KEY] = _uiState.value.copy(age = age)
     }
 }
--- app/src/main/java/com/example/mainscreen/User.kt
+++ app/src/main/java/com/example/mainscreen/User.kt
@@ -1,6 +1,10 @@
 package com.example.mainscreen
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class User(
     var name : String = "",
     var age : Int = 0
-)
+): Parcelable
